8d47affa28da2493ed7116c4fa117355adb8eaca
DedovichDK <Dedovichdenis@mail.ru>
2019-07-28 18:19:09 +0300

    WorkToOwner

--- Infro/InfroService/Owner.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Infro.InfroService
-{
-    public class Owner
-    {
-        public int OwnerID { get; set; }
-        public string Name { get; set; }
-        //public ICollection<Car> Cars { get; set; }
-    }
-}
